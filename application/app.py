906454c9abd05240b6a03324f2597317d4a18362
Alessio Liu <kuo@null.net>
2024-02-01 22:08:49 +0100

    Whitepaper release
    Finished the whitepaper draft /templates/index.html

--- application/app.py
+++ application/app.py
@@ -1,14 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
-# Create a Flask application instance
 app = Flask(__name__)
 
-# Define a route for the home page
 @app.route('/')
 def home():
-    return 'Hello, Flask!'
+    return render_template('index.html')
 
-# Run the application if the script is executed directly
 if __name__ == '__main__':
-    # Set the debug mode to True for development
     app.run(debug=True)
